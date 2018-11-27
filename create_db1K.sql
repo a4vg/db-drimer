a856c46878b97473043dc24d8c57cf81582fb2b5
Alessia Yi <alessia.yi@utec.edu.pe>
2018-11-27 15:31:37 -0500

    Modificado a filas en csv

--- create_db1K.sql
+++ create_db1K.sql
@@ -14,15 +14,16 @@ CREATE TABLE Tiene(
   no_recibo int,
   producto_id varchar (10),
   tienda_id int,
+  cantidad int,
   PRIMARY KEY (no_recibo, producto_id, tienda_id),
   FOREIGN KEY (no_recibo) REFERENCES Pedido,
   FOREIGN KEY (producto_id) REFERENCES Producto,
   FOREIGN KEY (tienda_id) REFERENCES Tienda
 );
 
-CREATE TABLE Mueble(
+CREATE TABLE Sofa(
   producto_id varchar(10),
-  tipo varchar(255),
+  modelo varchar(255),
   PRIMARY KEY (producto_id),
   FOREIGN KEY (producto_id) REFERENCES Producto
 );
@@ -60,27 +61,27 @@ CREATE TABLE Representante (
 );
 
 CREATE TABLE Cliente (
-  dni varchar (8),
   nombre varchar (255),
   apellido varchar (255),
-  telefono int,
   direccion varchar (500),
+  dni varchar (8),
+  telefono int,
   PRIMARY KEY (dni)
 );
 
 CREATE TABLE Producto (
   id varchar(10),
-  precio numeric(2),
   detalle varchar(500),
+  precio numeric(2),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE Empresa (
-  ruc varchar (11),
-  telefono varchar (9),
-  direccion varchar (500),
+CREATE TABLE PersonaJuridica (
   nombre varchar (255),
-  cliente_dni varchar (8),
+  direccion varchar (500),
+  ruc varchar (11),
+  representante_dni varchar (8),
+  telefono varchar (9),
   PRIMARY KEY (ruc, cliente_dni),
   FOREIGN KEY (cliente_dni) REFERENCES Cliente
 );
@@ -106,15 +107,16 @@ CREATE TABLE Tienei(
   no_recibo int,
   producto_id varchar (10),
   tienda_id int,
+  cantidad int,
   PRIMARY KEY (no_recibo, producto_id, tienda_id),
   FOREIGN KEY (no_recibo) REFERENCES Pedido,
   FOREIGN KEY (producto_id) REFERENCES Producto,
   FOREIGN KEY (tienda_id) REFERENCES Tienda
 );
 
-CREATE TABLE Mueblei(
+CREATE TABLE Sofai(
   producto_id varchar(10),
-  tipo varchar(255),
+  modelo varchar(255),
   PRIMARY KEY (producto_id),
   FOREIGN KEY (producto_id) REFERENCES Producto
 );
@@ -152,27 +154,27 @@ CREATE TABLE Representantei (
 );
 
 CREATE TABLE Clientei (
-  dni varchar (8),
   nombre varchar (255),
   apellido varchar (255),
-  telefono int,
   direccion varchar (500),
+  dni varchar (8),
+  telefono int,
   PRIMARY KEY (dni)
 );
 
 CREATE TABLE Productoi (
   id varchar(10),
-  precio numeric(2),
   detalle varchar(500),
+  precio numeric(2),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE Empresai (
-  ruc varchar (11),
-  telefono varchar (9),
-  direccion varchar (500),
+CREATE TABLE PersonaJuridicai (
   nombre varchar (255),
-  cliente_dni varchar (8),
+  direccion varchar (500),
+  ruc varchar (11),
+  representante_dni varchar (8),
+  telefono varchar (9),
   PRIMARY KEY (ruc, cliente_dni),
   FOREIGN KEY (cliente_dni) REFERENCES Cliente
 );
@@ -194,8 +196,9 @@ CREATE INDEX Colchoni_plazas ON Colchoni (plazas);
 CREATE INDEX Tienei_no_recibo ON Tienei (no_recibo);
 CREATE INDEX Tienei_producto_id ON Tienei (producto_id);
 CREATE INDEX Tienei_tienda_id ON Tienei (tienda_id);
-CREATE INDEX Mueblei_producto_id ON Mueblei (producto_id);
-CREATE INDEX Mueblei_tipo ON Mueblei (tipo);
+CREATE INDEX Tienei_cantidad ON Tienei (cantidad);
+CREATE INDEX Sofai_producto_id ON Sofai (producto_id);
+CREATE INDEX Sofai_tipo ON Sofai (tipo);
 CREATE INDEX PersonaNaturali_dni ON PersonaNaturali (dni);
 CREATE INDEX Tiendai_id ON Tiendai (id);
 CREATE INDEX Tiendai_direccion ON Tiendai (direccion);
@@ -213,18 +216,18 @@ CREATE INDEX Clientei_direccion ON Clientei (direccion);
 CREATE INDEX Productoi_id ON Productoi (id);
 CREATE INDEX Productoi_precio ON Productoi (precio);
 CREATE INDEX Productoi_detalle ON Productoi (detalle);
-CREATE INDEX Empresai_ruc ON Empresai (ruc);
-CREATE INDEX Empresai_telefono ON Empresai (telefono);
-CREATE INDEX Empresai_direccion ON Empresai (direccion);
-CREATE INDEX Empresai_nombre ON Empresai (nombre);
-CREATE INDEX Empresai_cliente_dni ON Empresai (cliente_dni);
+CREATE INDEX PersonaJuridicai_ruc ON PersonaJuridicai (ruc);
+CREATE INDEX PersonaJuridicai_telefono ON PersonaJuridicai (telefono);
+CREATE INDEX PersonaJuridicai_direccion ON PersonaJuridicai (direccion);
+CREATE INDEX PersonaJuridicai_nombre ON PersonaJuridicai (nombre);
+CREATE INDEX PersonaJuridicai_representante_dni ON PersonaJuridicai (representante_dni);
 CREATE INDEX Catalogoi_producto_id ON Catalogoi (producto_id);
 CREATE INDEX Catalogoi_tienda_id ON Catalogoi (tienda_id);
 CREATE INDEX Catalogoi_stock ON Catalogoi (stock);
 
 \d+ Colchoni
 \d+ Tienei
-\d+ Mueblei
+\d+ Sofai
 \d+ PersonaNaturali
 \d+ Tiendai
 \d+ Pedidoi
@@ -232,5 +235,5 @@ CREATE INDEX Catalogoi_stock ON Catalogoi (stock);
 \d+ Representantei
 \d+ Clientei
 \d+ Productoi
-\d+ Empresai
+\d+ PersonaJuridicai
 \d+ Catalogoi
